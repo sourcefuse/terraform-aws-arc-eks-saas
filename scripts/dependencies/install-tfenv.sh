f4e04d907f0c26f8d37d06ece453e7c1106e36ee
HarshitSF <harshit.kumar@sourcefuse.com>
2024-02-20 20:23:20 +0530

    workflow updated

--- scripts/dependencies/install-tfenv.sh
+++ scripts/dependencies/install-tfenv.sh
@@ -6,8 +6,8 @@ tfenv_path="$HOME/.tfenv"
 
 if ! command -v tfenv &> /dev/null; then
   git clone --depth=1 https://github.com/tfutils/tfenv.git $tfenv_path
-  rm -rf /usr/local/bin/terraform
-  ln -sf $tfenv_path/bin/* /usr/local/bin
+  sudo rm -rf /usr/local/bin/terraform
+  sudo ln -sf $tfenv_path/bin/* /usr/local/bin
   tfenv use latest
 else
   printf "tfenv already exists...\n"
