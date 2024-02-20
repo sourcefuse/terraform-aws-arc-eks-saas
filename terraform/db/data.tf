eb53d7279a53a52921bb795760e1e922e63f948c
HarshitSF <harshit.kumar@sourcefuse.com>
2024-02-20 14:15:53 +0530

    Added DB Module

---
+++ terraform/db/data.tf
@@ -0,0 +1,47 @@
+################################################################################
+## account
+################################################################################
+data "aws_partition" "this" {}
+
+data "aws_ssm_parameter" "terraform_state_bucket" {
+  name = "/${var.namespace}/${var.environment}/terraform-state-bucket"
+}
+
+################################################################################
+## network
+################################################################################
+data "aws_caller_identity" "current" {}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+
+  config = {
+    region = var.region
+    key    = "network/terraform.tfstate"
+    bucket = data.aws_ssm_parameter.terraform_state_bucket.value
+  }
+
+}
+
+data "aws_vpc" "vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["${var.namespace}-${var.environment}-vpc"]
+  }
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name = "tag:Type"
+
+    values = ["private"]
+  }
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name = "tag:Type"
+
+    values = ["public"]
+  }
+}
