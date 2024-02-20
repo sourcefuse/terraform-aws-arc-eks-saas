715f2853347b2b754a3ee410d8db9fdabfc3623d
HarshitSF <harshit.kumar@sourcefuse.com>
2024-02-20 12:24:43 +0530

    Added network module

---
+++ terraform/network/main.tf
@@ -0,0 +1,83 @@
+################################################################
+## defaults
+################################################################
+terraform {
+  required_version = "~> 1.3"
+
+  required_providers {
+    aws = {
+      version = "~> 5.0"
+      source  = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = var.region
+}
+
+################################################################
+## Tag
+################################################################
+module "tags" {
+  source = "git::https://github.com/sourcefuse/terraform-aws-refarch-tags?ref=1.2.5"
+
+  environment = var.environment
+  project     = var.namespace
+
+}
+
+################################################################
+## Network
+################################################################
+module "network" {
+  source  = "sourcefuse/arc-network/aws"
+  version = "2.6.10"
+
+  environment = var.environment
+  namespace   = var.namespace
+
+  availability_zones          = var.availability_zones
+  vpc_ipv4_primary_cidr_block = var.vpc_ipv4_primary_cidr_block
+
+  client_vpn_authorization_rules = var.client_vpn_authorization_rules
+  custom_subnets_enabled         = var.is_custom_subnet_enabled
+  client_vpn_enabled             = var.client_vpn_enabled
+
+
+  vpc_endpoint_config = {
+    s3         = var.enable_s3_endpoint
+    kms        = var.enable_kms_endpoint
+    cloudwatch = var.enable_cloudwatch_endpoint
+    elb        = var.enable_elb_endpoint
+    dynamodb   = var.enable_dynamodb_endpoint
+    ec2        = var.enable_ec2_endpoint
+    sns        = var.enable_sns_endpoint
+    sqs        = var.enable_sqs_endpoint
+    ecs        = var.enable_ecs_endpoint
+    rds        = var.enable_rds_endpoint
+  }
+
+  custom_nat_gateway_enabled          = var.custom_nat_gateway_enabled
+  gateway_endpoint_route_table_filter = ["*private*"]
+
+  custom_private_subnets = [
+    for idx in range(var.private_subnet_count) : {
+      name              = "${var.namespace}-${var.environment}-private-subnet-private-${var.region}${element(["a", "b", "c", "d", "e"], idx)}"
+      availability_zone = "${var.region}${element(["a", "b", "c", "d", "e"], idx)}"
+      cidr_block        = var.custom_private_subnet_ids[idx]
+    }
+  ]
+  custom_public_subnets = [
+    for idx in range(var.public_subnet_count) : {
+      name              = "${var.namespace}-${var.environment}-public-subnet-public-${var.region}${element(["a", "b", "c", "d", "e"], idx)}"
+      availability_zone = "${var.region}${element(["a", "b", "c", "d", "e"], idx)}"
+      cidr_block        = var.custom_public_subnet_ids[idx]
+    }
+  ]
+
+  tags = module.tags.tags
+}
+
---
+++ terraform/network/variables.tf
@@ -0,0 +1,163 @@
+################################################################
+## Default
+################################################################
+variable "region" {
+  description = "AWS Region"
+  default     = "us-east-1"
+  type        = string
+}
+
+variable "environment" {
+  type        = string
+  default     = "dev"
+  description = "ID element. Usually used for region e.g. 'uw2', 'us-west-2', OR role 'prod', 'staging', 'dev', 'UAT'"
+}
+
+variable "namespace" {
+  type        = string
+  description = "Namespace for the resources."
+  default     = "arc-saas"
+}
+
+#################################################################
+## Networking
+#################################################################
+
+variable "availability_zones" {
+  description = "List of availability zones to deploy resources in."
+  type        = list(string)
+
+  default = [
+    "us-east-1a",
+    "us-east-1b"
+  ]
+}
+
+variable "vpc_ipv4_primary_cidr_block" {
+  description = "CIDR block for the VPC to use."
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "is_custom_subnet_enabled" {
+  type        = bool
+  default     = false
+  description = "Enable to create subnets with custom cidr range"
+}
+
+variable "private_subnet_count" {
+  type        = number
+  default     = 2
+  description = "Number of private subnets required"
+}
+
+variable "public_subnet_count" {
+  type        = number
+  default     = 2
+  description = "Number of public subnets required"
+}
+
+variable "custom_private_subnet_ids" {
+  type        = list(string)
+  default     = []
+  description = "list of CIDR block for private subnets"
+}
+
+variable "custom_public_subnet_ids" {
+  type        = list(string)
+  default     = []
+  description = "list of CIDR block for public subnets"
+}
+
+variable "custom_create_aws_network_acl" {
+  type        = bool
+  description = "This indicates whether to create aws network acl or not"
+  default     = true
+}
+
+variable "custom_nat_gateway_enabled" {
+  description = "Enable the NAT Gateway between public and private subnets"
+  type        = bool
+  default     = true
+}
+
+variable "client_vpn_enabled" {
+  type        = bool
+  description = "Enable client VPN endpoint"
+  default     = false
+}
+
+variable "client_vpn_authorization_rules" {
+  type        = list(map(any))
+  default     = []
+  description = "List of objects describing the authorization rules for the client vpn"
+}
+
+################################################################################
+## vpc endpoint
+################################################################################
+variable "vpc_endpoints_enabled" {
+  type        = bool
+  description = "Enable VPC endpoints."
+  default     = true
+}
+
+variable "vpc_endpoint_type" {
+  type        = string
+  description = "The VPC endpoint type, Gateway, GatewayLoadBalancer, or Interface."
+  default     = "Interface"
+}
+
+variable "enable_s3_endpoint" {
+  type        = bool
+  description = "Enable S3 endpoints"
+  default     = true
+}
+
+variable "enable_kms_endpoint" {
+  type        = bool
+  description = "Enable KMS endpoints"
+  default     = true
+}
+
+variable "enable_cloudwatch_endpoint" {
+  type        = bool
+  description = "Enable CloudWatch endpoints"
+  default     = true
+}
+
+variable "enable_elb_endpoint" {
+  type        = bool
+  description = "Enable ELB endpoints"
+  default     = true
+}
+
+variable "enable_dynamodb_endpoint" {
+  type        = bool
+  description = "Enable DynamoDB endpoints"
+  default     = true
+}
+
+variable "enable_kms_endpoint" {
+  type        = bool
+  description = "Enable EC2 endpoints"
+  default     = true
+}
+
+variable "enable_sns_endpoint" {
+  type        = bool
+  description = "Enable SNS endpoints"
+  default     = true
+}
+
+variable "enable_ecs_endpoint" {
+  type        = bool
+  description = "Enable ECS endpoints"
+  default     = true
+}
+
+variable "enable_rds_endpoint" {
+  type        = bool
+  description = "Enable RDS endpoints"
+  default     = true
+}
