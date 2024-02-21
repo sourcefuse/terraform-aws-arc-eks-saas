9dcf587cf535c5e42c3d5a43b177f9217ea0c4e4
HarshitSF <harshit.kumar@sourcefuse.com>
2024-02-21 06:50:09 +0530

    workflow updated

--- scripts/update-backend-config.sh
+++ scripts/update-backend-config.sh
@@ -28,8 +28,6 @@ REGION="us-east-1"
 
 TF_STATE_BUCKET=$(aws ssm get-parameter --name "/${NAMESPACE}/${ENVIRONMENT}/terraform-state-bucket" --query 'Parameter.Value' --region "$REGION" --output text 2>/dev/null)
 TF_STATE_TABLE=$(aws ssm get-parameter --name "/${NAMESPACE}/${ENVIRONMENT}/terraform-state-dynamodb-table" --query 'Parameter.Value' --region "$REGION" --output text 2>/dev/null)
-#TF_STATE_BUCKET="arc-saas-dev-terraform-state-19aon0"
-#TF_STATE_TABLE="arc-saas-dev-terraform-state-lock"
 
 update_backend () {
     cd terraform/$DIRECTORY
--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -11,7 +11,7 @@ terraform {
     }
   }
 
-  backend "s3" {}
+  //backend "s3" {}
 }
 
 provider "aws" {
