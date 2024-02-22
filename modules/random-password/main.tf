212a313f07df349a64954fa112f437a3413cee39
HarshitSF <harshit.kumar@sourcefuse.com>
2024-02-22 19:47:05 +0530

    provider updated

--- modules/random-password/main.tf
+++ modules/random-password/main.tf
@@ -3,10 +3,12 @@
 ################################################################
 terraform {
   required_version = "~> 1.4"
-  aws = {
+  required_providers {
+    aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
 }
 
 
--- terraform/db/main.tf
+++ terraform/db/main.tf
@@ -4,12 +4,12 @@
 terraform {
   required_version = "~> 1.4"
 
-  # required_providers {
-  #   aws = {
-  #     version = "~> 5.0"
-  #     source  = "hashicorp/aws"
-  #   }
-  # }
+  required_providers {
+    aws = {
+      version = "~> 5.0"
+      source  = "hashicorp/aws"
+    }
+  }
 
   backend "s3" {}
 }
