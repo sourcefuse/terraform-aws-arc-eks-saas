299b62efecebe38c8677a672b0b41496ed26797f
HarshitSF <harshit.kumar@sourcefuse.com>
2024-02-22 17:14:20 +0530

    provider updated

--- terraform/db/main.tf
+++ terraform/db/main.tf
@@ -2,14 +2,6 @@
 ## defaults
 ################################################################################
 terraform {
-
-  required_providers {
-    aws = {
-      version = "~> 4.0"
-      source  = "hashicorp/aws"
-    }
-  }
-
   backend "s3" {}
 }
 
