de70c97317fc6e0183517e57a3cef639bc002762
HarshitSF <harshit.kumar@sourcefuse.com>
2024-02-22 23:57:27 +0530

    workflow updated

--- terraform/bootstrap/output.tf
+++ terraform/bootstrap/output.tf
@@ -1,19 +1,23 @@
 output "state_bucket_arn" {
+  sensitive = true
   value       = module.bootstrap.bucket_arn
   description = "State bucket ARN"
 }
 
 output "state_bucket_name" {
+  sensitive = true
   value       = module.bootstrap.bucket_name
   description = "State bucket name"
 }
 
 output "state_lock_table_arn" {
+  sensitive = true
   value       = module.bootstrap.dynamodb_arn
   description = "State lock table ARN"
 }
 
 output "state_lock_table_name" {
+  sensitive = true
   value       = module.bootstrap.dynamodb_name
   description = "State lock table name"
 }
