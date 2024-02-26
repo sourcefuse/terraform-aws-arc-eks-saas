0490ce081f3677f85230d0f3deb072246cec1175
HarshitSF <harshit.kumar@sourcefuse.com>
2024-02-26 10:43:28 +0530

    Added OpenSearch Module

--- terraform/core-infra-pipeline/main.tf
+++ terraform/core-infra-pipeline/main.tf
@@ -65,9 +65,9 @@ module "deployment_pipeline" {
   stages = [
     { name = "Bootstrap", category = "Build", owner = "AWS", provider = "CodeBuild", input_artifacts = "source_output", output_artifacts = "", run_order = "2", project_name = "${module.initial_bootstrap.name}" },
     { name = "Networking", category = "Build", owner = "AWS", provider = "CodeBuild", input_artifacts = "source_output", output_artifacts = "", run_order = "3", project_name = "${module.networking_module_build_step_codebuild_project.name}" },
-    { name = "Opensearch", category = "Build", owner = "AWS", provider = "CodeBuild", input_artifacts = "source_output", output_artifacts = "", run_order = "4", project_name = "${module.opensearch_module_build_step_codebuild_project.name}" },
     { name = "Database", category = "Build", owner = "AWS", provider = "CodeBuild", input_artifacts = "source_output", output_artifacts = "", run_order = "4", project_name = "${module.rds_module_build_step_codebuild_project.name}" },
-    { name = "Elasticache", category = "Build", owner = "AWS", provider = "CodeBuild", input_artifacts = "source_output", output_artifacts = "", run_order = "4", project_name = "${module.elasticache_module_build_step_codebuild_project.name}" }
+    { name = "Elasticache", category = "Build", owner = "AWS", provider = "CodeBuild", input_artifacts = "source_output", output_artifacts = "", run_order = "4", project_name = "${module.elasticache_module_build_step_codebuild_project.name}" },
+    { name = "Opensearch", category = "Build", owner = "AWS", provider = "CodeBuild", input_artifacts = "source_output", output_artifacts = "", run_order = "4", project_name = "${module.opensearch_module_build_step_codebuild_project.name}" }
   ]
   tags = module.tags.tags
 
