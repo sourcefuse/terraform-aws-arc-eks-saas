db63a235422b18d8e33affb364741065138e2b2a
HarshitSF <harshit.kumar@sourcefuse.com>
2024-02-27 15:03:22 +0530

    module updated

--- terraform/eks-auth/locals.tf
+++ terraform/eks-auth/locals.tf
@@ -1,7 +1,7 @@
 locals {
   map_karpenter_iam_role = [
     {
-      groups    = ["system:nodes"]
+      groups    = ["system:nodes","system:bootstrappers"]
       role_arn  = "arn:aws:iam::${data.aws_caller_identity.this.account_id}:role/${data.aws_ssm_parameter.karpenter_role.value}"
       user_name = "system:node:{{EC2PrivateDNSName}}"
     }
