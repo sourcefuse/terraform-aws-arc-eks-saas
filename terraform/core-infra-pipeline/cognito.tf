e4657d5adae8845a9ff9524b6303467450bb4fe0
HarshitSF <harshit.kumar@sourcefuse.com>
2024-02-28 14:00:18 +0530

    Added Cognito pipeline stage

--- terraform/core-infra-pipeline/cognito.tf
+++ terraform/core-infra-pipeline/cognito.tf
@@ -19,63 +19,57 @@ module "cognito_module_build_step_role" {
 #############################################################################################
 ## Codebuild Project
 #############################################################################################
-resource "aws_codebuild_project" "cognito_module_build_step_codebuild_project" {
-  name           = "terraform-cognito-module-build-step-code-build-${var.namespace}-${var.environment}"
-  description    = "terraform cognito module build step module code build project"
-  build_timeout  = 480
-  queued_timeout = 480
+module "cognito_module_build_step_codebuild_project" {
+  source                            = "../../modules/codebuild"
+  name                              = "terraform-cognito-module-build-step-code-build-${var.namespace}-${var.environment}"
+  description                       = "terraform cognito module build step module code build project"
+  build_timeout                     = 480
+  queued_timeout                    = 480
+  service_role                      = module.cognito_module_build_step_role.arn
+  artifact_type                     = "CODEPIPELINE"
+  build_compute_type                = "BUILD_GENERAL1_SMALL"
+  build_image                       = "aws/codebuild/standard:6.0"
+  build_type                        = "LINUX_CONTAINER"
+  build_image_pull_credentials_type = "CODEBUILD"
+  environment_variables             = []
 
-  service_role = module.cognito_module_build_step_role.arn
+  source_type = "CODEPIPELINE"
+  buildspec = yamlencode({
+    version = "0.2"
 
-  artifacts {
-    type = "CODEPIPELINE"
-  }
+    phases = {
 
-  environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:6.0"
-    type                        = "LINUX_CONTAINER"
-    image_pull_credentials_type = "CODEBUILD"
-
-  }
-  source {
-    type = "CODEPIPELINE"
-    buildspec = yamlencode({
-      version = "0.2"
-
-      phases = {
-        install = {
-          commands = [
-            "curl -o /usr/local/bin/terraform.zip https://releases.hashicorp.com/terraform/1.7.1/terraform_1.7.1_linux_amd64.zip",
-            "unzip /usr/local/bin/terraform.zip -d /usr/local/bin/",
-            "terraform --version",
-          ]
-        }
-
-        pre_build = {
-          commands = [
-            "export PATH=$PWD/:$PATH",
-            "apt-get update -y && apt-get install -y jq unzip",
-            "cd terraform/cognito-user-pool",
-            "rm config.${var.environment}.hcl",
-            "sed -i 's/aws_region/${var.region}/g' config.txt",
-            "tf_state_bucket=$(aws ssm get-parameter --name \"/${var.namespace}/${var.environment}/terraform-state-bucket\" --query \"Parameter.Value\" --output text --region ${var.region})",
-            "tf_state_table=$(aws ssm get-parameter --name \"/${var.namespace}/${var.environment}/terraform-state-dynamodb-table\" --query \"Parameter.Value\" --output text --region ${var.region})",
-            "envsubst < config.txt > config.${var.environment}.hcl",
-
-          ]
-        }
-
-        build = {
-          commands = [
-            "terraform init --backend-config=config.${var.environment}.hcl",
-            "terraform plan --var-file=${var.environment}.tfvars",
-            "terraform apply --var-file=${var.environment}.tfvars -auto-approve",
-          ]
-        }
+      install = {
+        commands = [
+          "curl -o /usr/local/bin/terraform.zip https://releases.hashicorp.com/terraform/1.7.1/terraform_1.7.1_linux_amd64.zip",
+          "unzip /usr/local/bin/terraform.zip -d /usr/local/bin/",
+          "terraform --version",
+        ]
       }
-    })
-  }
+
+      pre_build = {
+        commands = [
+          "export PATH=$PWD/:$PATH",
+          "apt-get update -y && apt-get install -y jq unzip",
+          "cd terraform/cognito-user-pool",
+          "rm config.${var.environment}.hcl",
+          "sed -i 's/aws_region/${var.region}/g' config.txt",
+          "tf_state_bucket=$(aws ssm get-parameter --name \"/${var.namespace}/${var.environment}/terraform-state-bucket\" --query \"Parameter.Value\" --output text --region ${var.region})",
+          "envsubst < config.txt > config.${var.environment}.hcl",
+
+        ]
+      }
+
+
+      build = {
+        commands = [
+          "terraform init --backend-config=config.${var.environment}.hcl",
+          "terraform plan --var-file=${var.environment}.tfvars",
+          "terraform apply --var-file=${var.environment}.tfvars -auto-approve",
+        ]
+      }
+    }
+  })
+
   tags = module.tags.tags
 }
-
