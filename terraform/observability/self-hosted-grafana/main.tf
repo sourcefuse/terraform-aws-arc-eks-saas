f8da6bc42d35b211c23e3b84a1167ceb6567dade
HarshitSF <harshit.kumar@sourcefuse.com>
2024-03-04 17:49:02 +0530

    observability stack updated

--- terraform/observability/self-hosted-grafana/main.tf
+++ terraform/observability/self-hosted-grafana/main.tf
@@ -67,7 +67,7 @@ resource "aws_iam_role_policy_attachment" "prometheus_managed_role_attachment" {
   depends_on = [module.web_identity_iam_role]
 }
 
-resource "aws_iam_role_policy_attachment" "prometheus_managed_role_attachment" {
+resource "aws_iam_role_policy_attachment" "worker_node_role_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
   role       = "${var.namespace}-${var.environment}-eks-workers"
 }
