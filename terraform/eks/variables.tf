944521e0b7284c759bea05bc653e267562ab8244
HarshitSF <harshit.kumar@sourcefuse.com>
2024-03-05 14:37:47 +0530

    eks module updated

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -269,23 +269,23 @@ variable "aws_for_fluentbit" {
     {
       name = "cloudWatchLogs.autoCreateGroup"
       value = true
-    },
-    {
-      name = "volumes[2].name"
-      value = "fluentbits-config"
-    },
-    {
-      name = "volumes[2].configMap.name"
-      value = "fluent-bit-config"
-    },
-    {
-      name = "volumeMounts[2].name"
-      value = "fluentbits-config"
-    },
-    {
-      name = "volumeMounts[2].mountPath"
-      value = "/fluent-bit/etc/"
-    }]
+    }
+    # {
+    #   name = "volumes[2].name"
+    #   value = "fluentbits-config"
+    # },
+    # {
+    #   name = "volumes[2].configMap.name"
+    #   value = "fluent-bit-config"
+    # },
+    # {
+    #   name = "volumeMounts[2].name"
+    #   value = "fluentbits-config"
+    # },
+    # {
+    #   name = "volumeMounts[2].mountPath"
+    #   value = "/fluent-bit/etc/"
+    # }]
   }
 }
 
