1fe6ae1e6f82fbdebbf129635515f3d39abbf300
HarshitSF <harshit.kumar@sourcefuse.com>
2024-03-11 17:23:58 +0530

    terraform provider updated' '

--- samples/silo-tenant/terraform/provider.tf
+++ samples/silo-tenant/terraform/provider.tf
@@ -2,7 +2,7 @@
 ## defaults
 ################################################################################
 terraform {
-  required_version = "~> 1.4"
+  required_version = "~> 1.3"
 
   required_providers {
     aws = {
