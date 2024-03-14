3f2ba70cdcf2b60c6d91da150e759c78966ee468
HarshitSF <harshit.kumar@sourcefuse.com>
2024-03-14 19:42:48 +0530

    main.tf updated

--- terraform/billing/main.tf
+++ terraform/billing/main.tf
@@ -22,79 +22,75 @@ resource "helm_release" "kubecost" {
   namespace        = "kubecost"
   create_namespace = true
 
-  values = [
-    "${file("values.yaml")}"
-  ]
-
-  set = [ {
+  set {
     name  = "global.prometheus.enabled"
     value = false
-  },
+  }
 
-  {
+  set {
     name  = "global.prometheus.fqdn"
     value = ""
-  },
+  }
 
-  {
+  set {
     name  = "global.grafana.enabled"
     value = false
-  },
+  }
 
-  {
+  set {
     name  = "global.grafana.domainName"
     value = ""
-  },
+  }
 
-  {
+  set {
     name  = "global.amp.enabled"
     value = true
-  },
+  }
 
-  {
+  set {
     name  = "global.amp.prometheusServerEndpoint"
     value = "http://localhost:8005/workspaces/${data.aws_ssm_parameter.prometheus_workspace_id.value}"
-  },
+  }
 
-  {
+  set {
     name  = "global.amp.remoteWriteService"
     value = "https://aps-workspaces.${var.region}.amazonaws.com/workspaces/${data.aws_ssm_parameter.prometheus_workspace_id.value}/api/v1/remote_write"
-  },
+  }
 
-  {
+  set {
     name  = "global.amp.sigv4.region"
     value = var.region
-  },
+  }
 
-  {
+  set {
     name  = "global.savedReports.enabled"
     value = true
-  },
+  }
 
-  {
+  set {
     name  = "global.cloudCostReports.enabled"
     value = true
-  },
+  }
 
-  {
+  set {
     name  = "upgrade.toV2"
     value = true
-  },
+  }
 
-   {
+  set {
     name  = "sigV4Proxy.region"
     value = var.region
-  },
+  }
 
- {
+  set {
     name  = "sigV4Proxy.host"
     value = "aps-workspaces.${var.region}.amazonaws.com"
-  },
+  }
 
-  {
+  set {
     name  = "networkCosts.enabled"
     value = true
   }
-  ]
+
 
 }
