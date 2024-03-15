3a99f94cf458271b156bbffe2903bf60740fcb63
HarshitSF <harshit.kumar@sourcefuse.com>
2024-03-15 11:50:59 +0530

    data.tf updated

--- terraform/billing/cost-usage-report.tf
+++ terraform/billing/cost-usage-report.tf
@@ -1,44 +1,54 @@
 #########################################################################
 ## Cost & Usage Report
 #########################################################################
-# module "bucket_suffix" {
-#   source     = "../../modules/random-password"
-#   length     = 6
-#   is_special = false
-#   is_upper   = false
-# }
+module "bucket_suffix" {
+  source     = "../../modules/random-password"
+  length     = 6
+  is_special = false
+  is_upper   = false
+}
 
 
-# module "cur_bucket" {
-#    source  = "terraform-aws-modules/s3-bucket/aws"
-#    version = "~> 3.0"
+module "cur_bucket" {
+   source  = "terraform-aws-modules/s3-bucket/aws"
+   version = "~> 3.0"
 
-#    bucket = "${var.namespace}-${var.environment}-cur-bucket-${module.bucket_suffix.result}"
-#    acl    = "private"
+   bucket = "${var.namespace}-${var.environment}-cur-bucket-${module.bucket_suffix.result}"
+   acl    = "private"
 
-#    force_destroy = true
+   force_destroy = true
 
-#    # Bucket policies
-#    attach_policy           = true
-#    policy                  = data.aws_iam_policy_document.bucket_policy.json
+   # Bucket policies
+   attach_policy           = true
+   policy                  = data.aws_iam_policy_document.bucket_policy.json
 
-#    block_public_acls       = true
-#    block_public_policy     = true
-#    ignore_public_acls      = true
-#    restrict_public_buckets = true
+   block_public_acls       = true
+   block_public_policy     = true
+   ignore_public_acls      = true
+   restrict_public_buckets = true
 
-#    tags = module.tags.tags
-#  }
+   tags = module.tags.tags
+ }
 
-# ## CUR Report
-# resource "aws_cur_report_definition" "cur" {
-#   report_name                = local.cur_report_name
-#   time_unit                  = "DAILY"
-#   format                     = "Parquet"
-#   compression                = "Parquet"
-#   additional_schema_elements = ["RESOURCES"]
-#   s3_bucket                  = module.cur_bucket.s3_bucket_id
-#   s3_prefix                  = "reports"
-#   s3_region                  = var.region
-#   additional_artifacts       = ["ATHENA"]
-# }
+
+module "this" {
+  source = "nuuday/terraform-aws-cur"
+  version = "0.1.3"
+  use_existing_s3_bucket  = true
+  s3_bucket_name          = "${var.namespace}-${var.environment}-cur-bucket-${module.bucket_suffix.result}"
+  s3_bucket_prefix        = "reports"
+  s3_use_existing_kms_key = true
+  s3_kms_key_alias        = "aws/s3"
+
+  report_name      = local.cur_report_name
+  report_frequency = "HOURLY"
+  report_additional_artifacts = [
+    "ATHENA",
+  ]
+
+  report_format      = "Parquet"
+  report_compression = "Parquet"
+  report_versioning  = "OVERWRITE_REPORT"
+
+  tags = module.tags.tags
+}
--- terraform/billing/data.tf
+++ terraform/billing/data.tf
@@ -31,28 +31,28 @@ data "aws_iam_policy_document" "kubecost_sa_policy" {
 #######################################################################################
 ## CUR bucket policy
 #######################################################################################
-# data "aws_iam_policy_document" "bucket_policy" {
-#   statement {
-#     principals {
-#       type        = "Service"
-#       identifiers = ["billingreports.amazonaws.com"]
-#     }
-#     actions = [
-#       "s3:GetBucketAcl",
-#       "s3:GetBucketPolicy"
-#     ]
-#     resources = [module.cur_bucket.s3_bucket_arn]
-#   }
-#   statement {
-#     principals {
-#       type        = "Service"
-#       identifiers = ["billingreports.amazonaws.com"]
-#     }
-#     actions = [
-#       "s3:PutObject"
-#     ]
-#     resources = [
-#       "${module.cur_bucket.s3_bucket_arn}/*",
-#     ]
-#   }
-# }
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["billingreports.amazonaws.com"]
+    }
+    actions = [
+      "s3:GetBucketAcl",
+      "s3:GetBucketPolicy"
+    ]
+    resources = [module.cur_bucket.s3_bucket_arn]
+  }
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["billingreports.amazonaws.com"]
+    }
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = [
+      "${module.cur_bucket.s3_bucket_arn}/*",
+    ]
+  }
+}
