def2f1545f3d8e41c00f275c5af421b299ae0d86
HarshitSF <harshit.kumar@sourcefuse.com>
2024-03-18 11:06:53 +0530

    Lambda runtime updated

--- terraform/grafana/main.tf
+++ terraform/grafana/main.tf
@@ -1,8 +1,8 @@
 resource "helm_release" "prometheus_blackbox_exporter" {
 
   name             = "prometheus-blackbox-exporter"
-  repository       = "https://github.com/prometheus-community/helm-charts/tree/main/charts/prometheus-blackbox-exporter"
-  chart            = "prometheus-community/prometheus-blackbox-exporter"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "prometheus-blackbox-exporter"
   version          = "prometheus-blackbox-exporter-8.12.0"
   namespace        = "kubecost"
   create_namespace = false
