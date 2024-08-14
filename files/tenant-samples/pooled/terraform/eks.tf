0cc804248d0ec20ee58774b74d1fb08767a5a638
HarshitSF <harshit.kumar@sourcefuse.com>
2024-08-14 12:11:00 +0530

    helm config updated

--- files/tenant-samples/pooled/terraform/eks.tf
+++ files/tenant-samples/pooled/terraform/eks.tf
@@ -102,7 +102,7 @@ resource "kubernetes_namespace" "my_namespace" {
 # generate tenant specific helm values.yaml
 
 data "template_file" "helm_values_template" {
-  template = file("${path.module}/../application-helm-chart/values.yaml")
+  template = file("${path.module}/../application-helm-chart/values.yaml.template")
   vars = {
     NAMESPACE        = local.kubernetes_ns
     TENANT_NAME      = var.tenant_name
