28b51610737ba198fd8c3b74b02e4b3848beda4b
HarshitSF <harshit.kumar@sourcefuse.com>
2024-08-22 14:13:35 +0530

    sample tenant updated

--- files/tenant-samples/bridge/terraform/version.tf
+++ files/tenant-samples/bridge/terraform/version.tf
@@ -3,20 +3,23 @@ terraform {
 
   required_providers {
     aws = {
-      version = "~> 4.0"
       source  = "hashicorp/aws"
+      version = ">= 4.0"
     }
+
     opensearch = {
       source  = "opensearch-project/opensearch"
       version = "2.2.0"
     }
+
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.7.0"
     }
+    
     postgresql = {
-      source  = "ricochet1k/postgresql"
-      version = "1.20.2"
+      source  = "cyrilgdn/postgresql"
+      version = "1.12.0"
     }
     archive = {
       source  = "hashicorp/archive"
