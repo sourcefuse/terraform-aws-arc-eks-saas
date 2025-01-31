2c05b5bbe32635fc0b66f97fa3df4dabbb89696d
Harshit Kumar <69293204+HarshitSF@users.noreply.github.com>
2025-01-31 14:42:08 +0530

    Update main.tf

--- modules/eks-auth/main.tf
+++ modules/eks-auth/main.tf
@@ -41,7 +41,7 @@ resource "kubernetes_config_map_v1_data" "aws_auth" {
     # Convert to list, make distinct to remove duplicates, and convert to YAML as mapRoles is a YAML string.
     # Replace() removes double quotes on "strings" in YAML output.
     # Distinct() only applies the change once, not append every run.
-    mapRoles = replace(yamlencode(distinct(concat(yamldecode(data.kubernetes_config_map.aws_auth.data.mapRoles), yamldecode(local.new_role_yaml)))), "\"", "")
+    mapRoles = length(local.new_role_yaml) > 0 ? replace(yamlencode(distinct(concat(yamldecode(data.kubernetes_config_map.aws_auth.data.mapRoles), yamldecode(local.new_role_yaml)))), "\"", "")
     #mapUsers    = replace(yamlencode(distinct(concat(yamldecode(data.kubernetes_config_map.aws_auth.data.mapUsers), yamldecode(local.new_user_yaml)))), "\"", "")
     mapAccounts = length(local.new_account_yaml) > 0 ? replace(yamlencode(distinct(concat(yamldecode(coalesce(data.kubernetes_config_map.aws_auth.data.mapAccounts, "[]")), yamldecode(local.new_account_yaml)))), "\"", "") : ""
     mapUsers    = length(local.new_user_yaml) > 0 ? replace(yamlencode(distinct(concat(yamldecode(coalesce(data.kubernetes_config_map.aws_auth.data.mapUsers, "[]")), yamldecode(local.new_user_yaml)))), "\"", "") : ""
